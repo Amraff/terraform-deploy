adf61935bdc31efadf1cb410274fdf4aee467d62
Amraff <raphewkarim@gmail.com>
2025-02-20 10:18:07 +0000

    upload content to s3

--- provider.tf
+++ provider.tf
@@ -1,17 +1,17 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "5.87.0"
-#     }
-#   }
-# backend "s3" {
-#     bucket = "raff-2025-tf-statefile"
-#     key = "devops/root/s3.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.87.0"
+    }
+  }
+backend "s3" {
+    bucket = "raff-2025-tf-statefile"
+    key = "devops/root/s3.tfstate"
+    region = "us-east-1"
+  }
+}
 
-# provider "aws" {
-#   region = "us-east-1"
-# }
+provider "aws" {
+  region = "us-east-1"
+}
