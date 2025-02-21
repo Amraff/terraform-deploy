6c9f6a4cdafe629ea26c1528e103e675aba7e28d
Amraff <raphewkarim@gmail.com>
2025-02-21 22:51:12 +0000

    delete resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
-module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
+# module "lambda_function" {
+#   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "my-lambda2"
-  description   = "My 50% cool lambda function"
-  handler       = "index.lambda_handler"
-  runtime       = "python3.12"
+#   function_name = "my-lambda2"
+#   description   = "My 50% cool lambda function"
+#   handler       = "index.lambda_handler"
+#   runtime       = "python3.12"
 
-  source_path = "./lambda-code/src/lambda-function1"
-}
+#   source_path = "./lambda-code/src/lambda-function1"
+# }
